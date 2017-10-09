52752d052adcf91ad8ef7d3425a1e50207ebda8d
marinecourtin <marinecourtin@users.noreply.github.com>
2017-10-09 13:37:25 +0200

    Add function my_sum()

--- functions.R
+++ functions.R
@@ -13,11 +13,35 @@
 sum_column <- function(d, var) {
   # default value to return
   result <- NULL
-  x <- d[[var]] # alternative d$var ->
+  x <- d[[var]] # alternative d$var -> it worked for me ?? to check later
   if (!is.null(x)) {
     if (is.numeric(x)) {
       result <- sum(x)
     }
+  }
+  return(result)
+}
+
+
+# Sum values in a vector.
+#
+# ARGUMENTS:
+# x: a vector
+#
+# RETURN VALUE:
+# if the vector contains numbers, returns the sum of
+# all values; otherwise, returns NULL
+#
+# [YOUR FUNCTION HERE]
+my_sum <- function(x) {
+  result <- NULL
+  if (is.numeric(x)) {
+    total = 0
+    length = length(x)
+    for (i in 1:length) {
+      total = total + x[i]
+    }
+  result <- total
   }
   return(result)
 }
