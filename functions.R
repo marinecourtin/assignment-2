636a4b6939d78b9edc003287b7f36cbdf5ddee2a
marinecourtin <marinecourtin@users.noreply.github.com>
2017-10-14 13:41:40 +0200

    Add answer 3.c
    Create permutation tests between 2 groups

--- functions.R
+++ functions.R
@@ -157,3 +157,48 @@ randomize <- function(d, var) {
   # such as sepal width
     return(d)
 }
+
+
+
+# Perform a permutation test for two groups.
+#
+# ARGUMENTS:
+# d: a data frame or tibble
+# var: the name of the column in d on which the test statistic will be calculated,
+# provided as a string
+# grouping_var: the name of the column in d which gives the grouping
+# group1: the value of grouping_var corresponding to the first group
+# group2: the value of grouping_var corresponding to the second group
+# statistic: a function yielding a test statistic, which takes as input
+# a data frame, the name of a variable on which to calculate the
+# test statistic, the name of a grouping variable, the value of
+# the grouping variable corresponding to the first group, and
+# the value of the grouping variable corresponding to the second
+# group
+# n_samples: the number of permutation samples to draw (default: 9999)
+#
+# RETURN VALUE:
+#
+# A list containing two elements:
+#
+#  - observed: the value of statistic() in d
+#  - permuted: a vector containing the values of statistic() under n_samples
+# permutations
+#
+permutation_twogroups <- function(d, var, grouping_var, group1, group2, statistic,
+                                  n_samples=9999) {
+  observed_statistic <- statistic(d, var, grouping_var, group1, group2)
+  permutation_statistics <- rep(0, n_samples)
+  for (i in 1:n_samples) {
+    # we create a permutation using the randomize function
+    permutation <- randomize(d, var)
+    # we store the result of our statistic function on the permutation
+    # at index i in the vector permutation_statistics
+    permutation_statistics[i] <- statistic(permutation, var, grouping_var, group1, group2)
+  }
+  # we return a list for easy access to results for both observation on our
+  # original data and on permutations
+  result <- list(observed=observed_statistic,
+                 permuted=permutation_statistics)
+  return(result)
+}
